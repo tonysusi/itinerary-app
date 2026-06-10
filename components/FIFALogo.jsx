fff4239d5bec490c448f0957f62b700fb2c942d3
Tony Susi <email@tonysusi.co.nz>
2026-06-10 14:00:12 +1200

    Update FIFALogo component to set default className for improved styling consistency.

--- components/FIFALogo.jsx
+++ components/FIFALogo.jsx
@@ -2,12 +2,12 @@
  * FIFA World Cup 2026 Logo
  * Source: https://commons.wikimedia.org/wiki/File:2026_FIFA_World_Cup_emblem.svg
  */
-export default function FIFALogo({ className = "", grayscale = true }) {
+export default function FIFALogo({ className = "h-8 w-8 object-contain", grayscale = true }) {
   return (
     <img
       src="https://upload.wikimedia.org/wikipedia/commons/1/17/2026_FIFA_World_Cup_emblem.svg"
       alt="FIFA World Cup 2026"
-      className={`max-w-full h-auto ${className}`}
+      className={className}
       style={grayscale ? { filter: "grayscale(100%) contrast(1.2)" } : undefined}
     />
   );
